a17fd3c7c86fec5abe1be9a06a2e9a53bf7127e3
Nguyễn Ngọc Anh <ngocanhnckh@MacBook-cua-Nguyen.local>
2021-10-08 10:27:48 +0700

    Added commit for labwork 1234

--- app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -1,6 +1,8 @@
 package vn.edu.usth.weather;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.util.Log;
 import android.widget.TextView;
 
 import android.os.Bundle;
@@ -10,24 +12,42 @@ public class WeatherActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.i("Weather","onCreate");
         setContentView(R.layout.activity_weather);
         TextView textView = (TextView)findViewById(R.id.textView);
         textView.setText("Hello mr.Son!");
+        ForecastFragment firstFragment = new ForecastFragment();
+        getSupportFragmentManager().beginTransaction().add(R.id.container, firstFragment).commit();
+
     }
-    public void onPause(){
-        super.onPause();
-    }
-    public void onResume(){
-        super.onResume();
-    }
-    public void onStart(){
+    @Override
+    protected void onStart(){
         super.onStart();
+        Log.i("Weather", "On Start");
     }
-    public void onStop(){
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.i("Weather", "On Resume");
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.i("Weather", "On Pause");
+    }
+
+    @Override
+    protected void onStop(){
         super.onStop();
+        Log.i("Weather", "On Stop");
     }
-    public void onDestroy(){
+
+    @Override
+    protected void onDestroy(){
         super.onDestroy();
+        Log.i("Weather", "On Destroy");
     }
 }
 
