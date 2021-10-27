38590603df48f57da815ed5de65ab4556f27bad1
Nguyễn Ngọc Anh <ngocanhnckh@MacBook-cua-Nguyen.local>
2021-10-27 13:28:46 +0700

    Added my favorite song to labwork 11

--- app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -1,7 +1,5 @@
 package vn.edu.usth.weather;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.util.Log;
 import android.widget.TextView;
 import android.widget.LinearLayout;
@@ -10,6 +8,14 @@ import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 import com.google.android.material.tabs.TabLayout;
+import android.media.MediaPlayer;
+import android.widget.Toast;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class WeatherActivity extends AppCompatActivity {
 
@@ -25,12 +31,45 @@ public class WeatherActivity extends AppCompatActivity {
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tab);
         tabLayout.setupWithViewPager(viewPager);
 
-
+        //Add Media Player
+        MediaPlayer player = MediaPlayer.create(WeatherActivity.this, R.raw.situvoismamere);
+        player.start();
     }
+
     @Override
     protected void onStart(){
         super.onStart();
+        copyFileToExternalStorage(R.raw.situvoismamere,"situvoismamere.mp3");
         Log.i("Weather", "On Start");
+    }
+
+    private void copyFileToExternalStorage(int resid, String resname){
+        try{
+            File file = new File(getExternalFilesDir(null),resname);
+            InputStream s = getApplicationContext().getResources().openRawResource(resid);
+            OutputStream o = new FileOutputStream(file);
+            byte[] buff = new byte[1024*2];
+            int read = 0;
+            try{
+                while ((read = s.read(buff)) > 0){
+                    o.write(buff,0,read);
+                }
+            }
+            finally {
+                Toast toast = Toast.makeText(getApplicationContext(), file.getAbsolutePath(), Toast.LENGTH_LONG);
+                toast.show();
+                s.close();
+                o.close();
+            }
+        }
+        catch (FileNotFoundException e){
+            e.printStackTrace();
+            Toast toast = Toast.makeText(getApplicationContext(), "File not found", Toast.LENGTH_LONG);
+            toast.show();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
     }
 
     @Override
